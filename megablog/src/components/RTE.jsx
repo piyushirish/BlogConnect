e6e4da6646de0683c6ffa8320daec8188834dac9
piyushpal <piyushpal9532@gmail.com>
2025-03-24 15:48:07 +0530

    rte changes

--- megablog/src/components/RTE.jsx
+++ megablog/src/components/RTE.jsx
@@ -13,7 +13,7 @@ export default function RTE({name, control, label, defaultValue =""}) {
     control={control}
     render={({field: {onChange}}) => (
         <Editor
-        apiKey = {Process.env.REACT_TINYMCE_EDITOR_KEY}
+        apiKey = {import.meta.env.VITE_TINYMCE_EDITOR_KEY}
         initialValue={defaultValue}
         init={{
             initialValue: defaultValue,
